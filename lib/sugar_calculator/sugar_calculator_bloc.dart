20b6b47cc8257ca5fadc343aa792356e58c28f40
Marcin Stepnowski <le0n.uo@gmail.com>
2023-04-26 15:53:30 +0200

    ✨ feat: add toggle unit feature to SugarCalculatorBloc

--- lib/sugar_calculator/sugar_calculator_bloc.dart
+++ lib/sugar_calculator/sugar_calculator_bloc.dart
@@ -7,11 +7,29 @@ class CalculateSugarEvent {
   CalculateSugarEvent({required this.sugarContent, required this.targetSugar});
 }
 
-class SugarCalculatorBloc extends Bloc<CalculateSugarEvent, double> {
-  SugarCalculatorBloc() : super(0) {
+class ToggleUnitEvent {
+  final bool isLiquid;
+
+  ToggleUnitEvent({required this.isLiquid});
+}
+
+class SugarCalculatorState {
+  final double result;
+  final bool isLiquid;
+
+  SugarCalculatorState({required this.result, required this.isLiquid});
+}
+
+class SugarCalculatorBloc extends Bloc<dynamic, SugarCalculatorState> {
+  SugarCalculatorBloc()
+      : super(SugarCalculatorState(result: 0, isLiquid: true)) {
     on<CalculateSugarEvent>((event, emit) {
       final result = (event.targetSugar * 100) / event.sugarContent;
-      emit(result);
+      emit(SugarCalculatorState(result: result, isLiquid: state.isLiquid));
+    });
+
+    on<ToggleUnitEvent>((event, emit) {
+      emit(SugarCalculatorState(result: state.result, isLiquid: event.isLiquid));
     });
   }
 }
